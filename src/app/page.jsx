c728b653a8d851391ceecbbc9ec65068ef50df0e
YogaA04 <YogaA04@github.com>
2024-05-18 12:43:21 +0700

    fixed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,8 +5,11 @@ import SlideShow from "@/components/AnimeList/slideShow";
 const Page = async () => {
 
   const topAnime = await getAnimeResponse("top/anime", "limit=12")
-  const recommendadAnime = await getNestedAnimeResponse("recommendations/anime", "entry")
-  const recomended = reproduce(recommendadAnime, 12)
+  let recommendadAnime = await getNestedAnimeResponse("recommendations/anime", "entry")
+
+  const first = ~~(Math.random() * (recommendadAnime.length - 12) + 1)
+  const last = first + 12
+  recommendadAnime = { data: recommendadAnime.slice(first, last) }
 
   return (
     <>
@@ -17,7 +20,7 @@ const Page = async () => {
 
       <section className="p-4">
         <Header link={'/rekomendasi'} linkTitile={'Lihat semua'} title={'Rekomendasi'} />
-        <SlideShow api={recomended} />
+        <SlideShow api={recommendadAnime} />
       </section>
     </>
   );
